d07af9b78cd56f447c57af0ae96471b6a462e896
majo-z <g00340481@gmit.ie>
2017-09-21 09:30:31 +0100

    Initial commit

---
+++ 02-current-time.go
@@ -0,0 +1,15 @@
+//Current time in Go
+//Adapted from https://tour.golang.org/welcome/4
+//Author: Marian Ziacik
+//Date: 21-sep 2017
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("The time is", time.Now())
+}
