c044e26c571c8393d0833e38e420030ccdd11da1
majo-z <g00340481@gmit.ie>
2017-09-21 01:10:06 +0100

    initial commit

---
+++ 01-hello-world.go
@@ -0,0 +1,12 @@
+//Hello, world! in Go
+//Adapted from https://tour.golang.org/welcome/1
+//Author: Marian Ziacik
+//Date: 19-sep 2017
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
