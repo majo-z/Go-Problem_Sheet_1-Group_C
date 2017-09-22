a7471dbdb090f6fb18def2f426585c03043b9a09
majo-z <g00340481@gmit.ie>
2017-09-22 01:17:46 +0100

    code change, added comments

--- 01-hello-world.go
+++ 01-hello-world.go
@@ -1,17 +1,28 @@
-//Hello, world! in Go
-//Adapted from https://tour.golang.org/welcome/1
+//Description: Program prints “Hello, world!” in Japanese (using Japanese characters) to the screen
 //Author: Marian Ziacik
 //Date: 19-sep 2017
+//Adapted from https://tour.golang.org/welcome/1
 
+// This file belongs to the package main - this means it will become an executable
 package main
 
+// The fmt package is used to print to the console, in this case
 import "fmt"
 
 func main() {
 	fmt.Println("こんにちは、世界！")
 
-//Another way in english
+	//Another way in english using concatenating
 	string_1 := "Hello"
-	string_2 := "World"
+	string_2 := "World!"
 	fmt.Println(string_1 + ", " + string_2)
+
+	//Different ways
+	var message string //message := "Hello, World"
+	message = "Hello, World"
+
+	fmt.Println(message)
+
+	//Printing "Hello, World" using Printf function - string formatting
+	fmt.Printf("Hello, World\n")
 }
--- 02-current-time.go
+++ 02-current-time.go
@@ -1,23 +1,28 @@
-//Current time in Go
-//Adapted from https://tour.golang.org/welcome/4
+//Description: Program prints the current time and date to the console
 //Author: Marian Ziacik
 //Date: 21-sep 2017
+//Adapted from https://tour.golang.org/welcome/4
 
 package main
 
 import (
 	"fmt"
-	"time"
+	"time" // The time package provides the current time and data
 )
 
 func main() {
-	fmt.Println("The time is", time.Now())
+	fmt.Println("The time and date are: ", time.Now())
 
-	//Formatting options
+	//Formatting & parsing options
 	//Adapted from https://gobyexample.com/time-formatting-parsing
+
+	//substituting printLine for fmt.Println
+	//now the printLine can be used intead of fmt.Println
 	printLine := fmt.Println
 
+	//substituting t for time.Now()
 	t := time.Now()
-	printLine(t.Format(time.RFC3339))
+	printLine(t.Format("The time and date are: " + time.RFC3339))
+	printLine(t.Format("Date: Mon Jan _2 2006, Time: 15:04:05 "))
 
 }
