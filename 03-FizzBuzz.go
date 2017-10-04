34826068702c4b8c686206957e28ee37046fc7b3
G00340481 <g00340481@gmit.ie>
2017-10-04 21:33:10 +0100

    change code, added code

--- 03-FizzBuzz.go
+++ 03-FizzBuzz.go
@@ -6,13 +6,16 @@ import (
 
 func main() {
 
-	outputNumbers := ""
-	//outputFizz := "Fizz"
-	//outputBuzz := "Buzz"
+	outputFizz := "Fizz"
+	outputBuzz := "Buzz"
 
 	for i := 1; i <= 100; i++ {
-		if outputNumbers == "" {
+		if i%3 == 0 {
+			fmt.Println(outputFizz)
+		} else if i%5 == 0 {
+			fmt.Println(outputBuzz)
+		} else {
 			fmt.Println(i)
 		}
-	}
-}
+	} //for
+} //main
