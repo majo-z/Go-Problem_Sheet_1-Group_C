51ea8a74399da2378e7a9dc96d58466d80c7493c
G00340481 <g00340481@gmit.ie>
2017-10-04 22:13:21 +0100

    added comment

--- 03-FizzBuzz.go
+++ 03-FizzBuzz.go
@@ -1,3 +1,11 @@
+//Description: Program prints the numbers from 1 to 100,
+//For multiples of three print Fizz instead of the number, and for multiples of five print Buzz.
+//For numbers that are multiples of both three and five print FizzBuzz.
+
+//Author: Marian Ziacik
+//Date: 4-oct 2017
+//Insired by video from https://www.youtube.com/watch?v=QPZ0pIK_wsc
+
 package main
 
 import (
@@ -11,13 +19,13 @@ func main() {
 
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println(outputFizz + outputBuzz)
+			fmt.Println(outputFizz + outputBuzz) //multiples of three and five print FizzBuzz
 		} else if i%3 == 0 {
-			fmt.Println(outputFizz)
+			fmt.Println(outputFizz) //multiples of three print Fizz
 		} else if i%5 == 0 {
-			fmt.Println(outputBuzz)
+			fmt.Println(outputBuzz) //multiples of five print Buzz
 		} else {
-			fmt.Println(i)
+			fmt.Println(i) //otherwise print number
 		}
 	} //for
 } //main
