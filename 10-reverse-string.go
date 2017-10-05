f115edab7d3f18b57381e9afe44d1391b363f7ca
G00340481 <g00340481@gmit.ie>
2017-10-05 18:18:43 +0100

    code change, commnets added

--- 10-reverse-string.go
+++ 10-reverse-string.go
@@ -1,7 +1,7 @@
 //Description: Write a function to reverse a string in Go.
 //Author: Marian Ziacik
 //Date: 5-oct 2017
-//Adopted from https://coderwall.com/p/fw6miw/reverse-text-in-golang
+//Adapted from https://coderwall.com/p/fw6miw/reverse-text-in-golang
 package main
 
 import "fmt"
@@ -9,18 +9,20 @@ import "fmt"
 func main() {
 	var myString string
 
-	fmt.Printf("Please enter your string:\n")
-	fmt.Scanf("%s", myString)
-	fmt.Println(myString)
-	fmt.Println(ReverseString(myString))
-
+	//ask and read user input
+	fmt.Printf("Please enter your string: ")
+	fmt.Scanf("%s", &myString)
+	//print the entered string, print the reversed string
+	fmt.Printf("Entered string: %s\n", myString)
+	fmt.Printf("Reversed string: %s\n", ReverseString(myString))
 }
 
-//function for string reverse
+//string reverse function
 func ReverseString(str string) string {
 	var reverse string
-
+	//get the length of the string
 	for i := len(str) - 1; i >= 0; i-- {
+		//add char from last position to first
 		reverse += string(str[i])
 	}
 	return reverse
