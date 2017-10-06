e302ac1419fed102e8b325614db4ea6258990e30
G00340481 <g00340481@gmit.ie>
2017-10-06 19:24:54 +0100

    adde more code and commnets

--- 07-palindrome-test.go
+++ 07-palindrome-test.go
@@ -1,20 +1,38 @@
+//Description: Program tests whether a string is a palindrome. A palindrome is a string that reads the same in reverse, e.g. radar.
+//Author: Marian Ziacik
+//Date: ^-oct 2017
+
+// Goolang Documentation: strings
+//https://golang.org/src/strings/example_test.go
 package main
 
 import (
 	"fmt"
-	"strings"
+	"strings" //package for handling strings, in this case used for strings.ToLower
 )
 
 func main() {
+	fmt.Printf("Strings are case insensitive\n")
+	fmt.Printf("========== HARD CODED TESTS ==========\n")
 	pal1 := "lEvel"
 	pal2 := "nooN"
 	pal3 := "Hello"
 	pal4 := "World"
+	pal5 := "radar"
 
-	fmt.Println(TestPalindrome(pal1))
-	fmt.Println(TestPalindrome(pal2))
-	fmt.Println(TestPalindrome(pal3))
-	fmt.Println(TestPalindrome(pal4))
+	fmt.Printf("The string %s is palindrome: %v\n", pal1, TestPalindrome(pal1))
+	fmt.Printf("The string %s is palindrome: %v\n", pal2, TestPalindrome(pal2))
+	fmt.Printf("The string %s is palindrome: %v\n", pal3, TestPalindrome(pal3))
+	fmt.Printf("The string %s is palindrome: %v\n", pal4, TestPalindrome(pal4))
+	fmt.Printf("The string %s is palindrome: %v\n", pal5, TestPalindrome(pal5))
+
+	fmt.Println()
+
+	fmt.Printf("========== USER INPUT TEST ==========\n")
+	var userInput string
+	fmt.Printf("Please enter your string:\n")
+	fmt.Scanf("%s%\n", &userInput)
+	fmt.Printf("The string %s is palindrome: %v\n", userInput, TestPalindrome(userInput))
 }
 
 //same funtion is used in reverse string example(interchangeable)
