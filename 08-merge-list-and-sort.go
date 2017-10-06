8e00474d90eb9e0dae74be8cc2c489e4f4840dcb
G00340481 <g00340481@gmit.ie>
2017-10-06 22:34:34 +0100

    added & code, added comments

--- 08-merge-list-and-sort.go
+++ 08-merge-list-and-sort.go
@@ -10,14 +10,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"sort"
+	"strings"
 )
 
-//import "bufio"
-//import "os"
-
 func main() {
+	scanner := bufio.NewScanner(os.Stdin) //standard input
+
+	fmt.Printf("=============== USER INPUT TEST ===============\n")
+	//ask and read the input
+	fmt.Println("Enter str1Array chars/numbers separated by space: ")
+	scanner.Scan()
+	string1 := scanner.Text()
+	str1Array := strings.Fields(string1) //strings.Fields() function  converts the string into an array.
+	fmt.Println("str1Array", str1Array)
+
+	fmt.Println("Enter str2Array chars/numbers separated by space: ")
+	scanner.Scan()
+	string2 := scanner.Text()
+	str2Array := strings.Fields(string2)
+	fmt.Println("str2Array", str2Array)
+
+	slice := make([]string, 0)                                //declare slice
+	slice = append(slice, append(str1Array, str2Array...)...) //append str1Array and str2Array
+	fmt.Println("slice", slice)                               //print the slice
+
+	sort.Strings(slice)                //sorts in-place
+	fmt.Println("slice Sorted", slice) //print the sorted slice
+
+	fmt.Println()
+
+	fmt.Printf("=============== HARD CODED TESTS ===============\n")
 	//Declare slices
 	slice1 := []float64{3.2, 6.3, 54.3}
 	slice2 := []float64{1.2, 4.5, 23.0}
@@ -36,24 +62,12 @@ func main() {
 	sort.Float64s(slice3)                //sorts in-place
 	fmt.Println("Slice3 Sorted", slice3) //print the sorted slice
 
-	fmt.Println()
-	//======================================================================
-
 	//==================== MORE EXAMPLES ===================================
-	//var slice4 []float64 = make([]float64, 5) //create slice of 5 elements, initialized to 0
+	/* //var slice4 []float64 = make([]float64, 5) //create slice of 5 elements, initialized to 0
 	//slice4 := make([]float64, 5) //shorthand
 	slice4 := make([]float64, len(slice1)+len(slice2)) //create slice of length of slice1 and slice2
 	fmt.Println(slice4)
 
-	/*
-	   scanner := bufio.NewScanner(os.Stdin)
-	   fmt.Println("Enter slice 1 numbers.")
-	   scanner.Scan()
-	   slice1String := scanner.Text()
-	   fmt.Println("Enter slice 2 numbers")
-	   scanner.Scan()
-	   slice2String := scanner.Text()
-	   fmt.Println(slice1String)
-	   fmt.Println(slice2String)
-	*/
-}
+	fmt.Println() */
+
+} //main
