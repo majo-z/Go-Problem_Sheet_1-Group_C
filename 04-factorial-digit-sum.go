f6f0f68c8c1d8260057974e6eb99e916fe375a41
G00340481 <g00340481@gmit.ie>
2017-10-06 18:33:47 +0100

    changed code, added comments

--- 04-factorial-digit-sum.go
+++ 04-factorial-digit-sum.go
@@ -1,4 +1,5 @@
 //Description: Program calculates the sum of the digits of the factorial of a number 100
+//Assuminf the numbers are integers only
 //Author: Marian Ziacik
 //Date: 5-oct 2017
 
@@ -18,15 +19,24 @@ import (
 
 func main() {
 
-	num := big.NewInt(100) // *Int
-	//fmt.Println(factorial(num))
+	var num int64
+	res := big.NewInt(num) //parse int64 to bigInt
+	fmt.Printf("Enter a number you wat to calculate the factorial of: ")
+	fmt.Scan(&num)
+	res.MulRange(1, num)                               //calculate factorial of entered number
+	fmt.Printf("Factorial of %d is: %d\n\n", num, res) //print as bigInt
 
-	num.MulRange(1, 100)                                         //same as 100 x 99 x 98 ... x 1
-	fmt.Printf("Factorial of %d is: %d\n", big.NewInt(100), num) //print factorial of 100
+	//=============================================================================
+
+	num2 := big.NewInt(100) // *Int
+	//fmt.Println(factorial(num2))
+
+	num2.MulRange(1, 100)                                         //same as 100 x 99 x 98 ... x 1
+	fmt.Printf("Factorial of %d is: %d\n", big.NewInt(100), num2) //print factorial of 100
 
 	//split to get every character in a slice ["1", "2", "3" ...]
-	//var digitsStr []string = strings.Split(num.String(), "")
-	digitsStr := strings.Split(num.String(), "") //shorthand
+	//var digitsStr []string = strings.Split(num2.String(), "")
+	digitsStr := strings.Split(num2.String(), "") //shorthand
 
 	total := 0 //for sum up individual digits
 	for _, value := range digitsStr {
