17fec93283f5a14ccd9810f23cdcb484baf49a72
G00340481 <g00340481@gmit.ie>
2017-10-06 18:44:01 +0100

    added code, added comments

--- 06-largest-and-smallest.go
+++ 06-largest-and-smallest.go
@@ -1,3 +1,8 @@
+//Description: Program that returns the largest and smallest elements in a array
+//Author: Marian Ziacik
+//Date: 6-oct 2017
+//The Go Programming Language Documents:
+// https://tour.golang.org/moretypes/6
 package main
 
 import "fmt"
@@ -9,6 +14,7 @@ func main() {
 
 //function takes array of integers and returns smallest and largest number of the array
 func largestSmallest(numbers []int) (int, int) { //enter array
+	//Initialize smallest and largest to 0
 	smallest := numbers[0]
 	largest := numbers[0]
 
@@ -20,6 +26,5 @@ func largestSmallest(numbers []int) (int, int) { //enter array
 			smallest = num
 		}
 	}
-
 	return smallest, largest
-}
+} //largestSmallest
